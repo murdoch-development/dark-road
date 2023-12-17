9ddd77c31c92443bdc4cdc4bb05ab0a6e960b9f1
Liam Chimes <stunbrick@gmail.com>
2023-12-17 20:23:43 +0200

    better title screen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -4,24 +4,33 @@ signal play_next_song
 
 var is_started = false
 var button_speed = 500
-var is_modulating = false
-var modulation = 1.0
+var slay_is_modulating = false
+var slay_modulation = 1.0
+var bright_lights_is_modulating = false
+var bright_lights_modulation = 1
 var is_moving = false
 
 func _ready():
 	$Black.visible = true
-	$Bright.visible = false
-	$Normal.visible = false
+	$BrightLights.visible = false
+	$NormalLights.visible = false
+	$CarPlain.visible = false
+	$CarLights.visible = false
 	$PlayButton.visible = true
+	$CarText.visible = false
 
 func _process(delta):
 	if Input.is_action_pressed("ui_select")\
 	or Input.is_action_pressed("ui_accept"):
 		start()
-	if is_modulating and modulation >= 0:
-		modulation -= delta / 2
-		$SlayButton.modulate = Color(1, 1, 1, modulation)
-	if is_moving and $LogoROADARK.position.y < 176:
+	if slay_is_modulating and slay_modulation >= 0:
+		slay_modulation -= delta / 2
+		$SlayButton.modulate = Color(1, 1, 1, slay_modulation)
+	if bright_lights_is_modulating and bright_lights_modulation >= 0:
+		bright_lights_modulation -= delta / 2
+		$BrightLights.modulate = Color(1, 1, 1, bright_lights_modulation)
+		print("light: ", bright_lights_modulation)
+	if is_moving and $LogoROADARK.position.y < 0:
 		$LogoROADARK.position.y += delta * button_speed
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
@@ -35,14 +44,15 @@ func start():
 	emit_signal("play_next_song")
 	$PlayButton.visible = false
 	$SlayButton.visible = true
-	is_modulating = true
+	slay_is_modulating = true
 	yield(get_tree().create_timer(9.5), "timeout")
 	$Black.visible = false
-	$Bright.visible = true
-	yield(get_tree().create_timer(0.5), "timeout")
-	$Bright.visible = false
-	$Normal.visible = true
-	yield(get_tree().create_timer(7), "timeout")
+	$BrightLights.visible = true
+	$NormalLights.visible = true
+	bright_lights_is_modulating = true
+	$CarLights.visible = true
+	yield(get_tree().create_timer(7.5), "timeout")
+	$LogoROADARK.visible = true
 	is_moving = true
 	yield(get_tree().create_timer(5), "timeout")
 	emit_signal("next_level")
